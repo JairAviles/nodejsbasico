8b2f5b25ae40fbd7ac13f6ab68dc720b0b9eccee
Jair Israel Aviles Eusebio <Jair.Israelavilaseusebio@nike.com>
2019-03-25 13:25:02 -0700

    fix minor issues

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call");
+
+call.withCallback("Jair Aviles", call.sync);
+
+call.withPromise("Jair Aviles")
+  .then(name=> {console.log(name)});
