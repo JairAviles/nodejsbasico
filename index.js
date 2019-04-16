7245a011811f12bed28a4ed4aeaa3b0005dc262f
Jair Israel Aviles Eusebio <Jair.Israelavilaseusebio@nike.com>
2019-04-15 22:09:13 -0700

    ejercicios clase fs

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const fs = require('fs')
+const ops = require('./src/fileops')
+
+let incValue;
+
+fs.readFile('./resources/number.txt', 'utf8', (err, text) => {
+  if(err) throw err;
+  const numbers = text.split('\n');
+  incValue = ops.incrementValues(numbers);
+  fs.writeFile('./resources/numbernew.txt', incValue.join('\n'), (err, response) => {
+    if(err) throw err;
+  });
+  
+});
+
+fs.readFile('./resources/name.txt', 'utf8', (err, text) => {
+  if(err) throw err;
+  const names = text.split('\n');
+  greetNames = ops.callNames(names);
+  greetNames.map(greet => console.log(greet));
+});
