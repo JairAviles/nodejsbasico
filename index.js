9d17f13ee862ce7bced144edd6ba4eb7c1f2c7fc
Jair Israel Aviles Eusebio <Jair.Israelavilaseusebio@nike.com>
2019-04-18 16:10:04 -0700

    Ejercicio de clase del modulo de readline de node

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const q = [
+  "Cual es tu primer nombre? ",
+  "Cual es tu primer apellido? ",
+  "Cual es tu edad? "
+];
+
+const AskQuestion = (rl, question) => {
+  return new Promise((res, err) => {
+    rl.question(question, answer => {
+      res(answer);
+    });
+  });
+}
+
+const AskForm = function(questions) {
+  return new Promise(async (res) => {
+    let results = [];
+    for (let i=0; i < questions.length; i++) {
+      const result = await AskQuestion(rl, questions[i]);
+      results.push(result);
+    }
+    rl.close();
+    res(results);
+  })
+}
+
+AskForm(q).then(q => {
+  console.log(`Hi ${q[0]} ${q[1]}, you are ${q[2]} years old`);
+});
