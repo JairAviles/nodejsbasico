0b517a1483d7275ea9fa25d80121dd0cc772dea1
Jair Israel Aviles Eusebio <Jair.Israelavilaseusebio@nike.com>
2019-04-19 22:33:19 -0700

    add debugger

--- src/counter.js
+++ src/counter.js
@@ -1,6 +1,5 @@
 module.exports = {
     from0to: function(n, inc) {
-        console.log(`Counting from 0 to ${n} by ${inc}:`);
         for(let i = 0; i < n; i += inc){
             console.log(i);
         }
