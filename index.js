52f7f19b9eb98a78e0c9ed8143d5d3a5e4e1f3dd
Jair Israel Aviles Eusebio <Jair.Israelavilaseusebio@nike.com>
2019-04-20 16:31:13 -0700

    Event emmiter exercise

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const greet = require('./src/greet');
+
+greet.emit('clap');
+greet.emit('greet', 'people');
+greet.emit('shout', 'LETS GO BLAZERS')
+greet.emit('call', 'Jair Aviles', (name) => {
+    console.log(`Hello, my name is ${name}. Nice to meet you.`)
+})
