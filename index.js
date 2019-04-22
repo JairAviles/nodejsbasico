338c5e94bb97707272853a2fb9b6b9531b5606b9
Jair Israel Aviles Eusebio <Jair.Israelavilaseusebio@nike.com>
2019-04-22 16:14:15 -0700

    add http server definition

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  fs.readFile('./resources/my_page.html', (err, data) =>{
+    if (err) { console.err(err); return; }
+    res.end(data);
+  });
+});
+
+console.log('Server running...')
+server.listen(3000);
