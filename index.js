c348fa5886b4c20b876ec615182c3ba7f305da9e
Jair Israel Aviles Eusebio <Jair.Israelavilaseusebio@nike.com>
2019-04-22 16:45:41 -0700

    Add node http server module example

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const http = require('http')
 const fs = require('fs')
 
+const hostname = '127.0.0.1'
+const port = 3000
+
 const server = http.createServer((req, res) => {
   fs.readFile('./resources/my_page.html', (err, data) =>{
     if (err) { console.err(err); return; }
@@ -9,4 +12,6 @@ const server = http.createServer((req, res) => {
 });
 
 console.log('Server running...')
-server.listen(3000);
+server.listen(port, hostname, () => {
+  console.log(`Running server: ${hostname}:${port}`)
+});
