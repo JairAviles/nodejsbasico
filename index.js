30dfd550ca30e0c0f28a3999efdb6ba91651a7be
Jair Israel Aviles Eusebio <Jair.Israelavilaseusebio@nike.com>
2019-04-22 20:00:54 -0700

    ejercicio express clase 1

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const express = require('express');
+const app = express();
+
+app.get('/app', (req, res) => {
+  res.send('I\'m in APP route');
+});
+
+app.get('/', (req, res) => {
+  res.send('I\'m in HOME route');
+});
+
+app.get('*', (req, res) => {
+  res.send('Not sure where I\'m');
+});
+
+app.listen(3000, () => {
+  console.log('Express server is listening in port 3000...');
+});
